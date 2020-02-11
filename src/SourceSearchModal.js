2253ebae733cf4e866a3563ed11e764736c6b878
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-02-11 18:25:53 +0100

    UIFC-124-plugin-mdSource change source

--- src/SourceSearchModal.js
+++ src/SourceSearchModal.js
@@ -12,7 +12,8 @@ class SourceSearchModal extends Component {
       connect: PropTypes.func.isRequired
     }).isRequired,
     modalRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-    onSourceSelected: PropTypes.func.isRequired,
+    // onSourceSelected: PropTypes.func.isRequired,
+    selectSource: PropTypes.func.isRequired,
     onClose: PropTypes.func.isRequired,
     open: PropTypes.bool,
     dataKey: PropTypes.string
@@ -25,7 +26,8 @@ class SourceSearchModal extends Component {
   }
 
   selectSource = (e, source) => {
-    this.props.onSourceSelected(source);
+    // this.props.onSourceSelected(source);
+    this.props.selectSource(source);
     this.props.onClose();
   };
 
