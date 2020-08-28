c76d602f7525a11939a0201d1afc7d1f3108b402
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-08-28 11:19:08 +0200

    Rename status 'wish' to 'request'

--- src/filterConfigData.js
+++ src/filterConfigData.js
@@ -5,11 +5,10 @@ const filterConfig = [
     cql: 'status',
     values: [
       { name: 'Active', cql: 'active' },
-      { name: 'Wish', cql: 'wish' },
-      { name: 'Negotiation', cql: 'negotiation' },
-      { name: 'Technical implementation', cql: 'technical implementation' },
-      { name: 'Deactivated', cql: 'deactivated' },
-      { name: 'Terminated', cql: 'terminated' }
+      { name: 'Request', cql: 'request' },
+      { name: 'Implementation', cql: 'implementation' },
+      { name: 'Closed', cql: 'closed' },
+      { name: 'Impossible', cql: 'impossible' }
     ],
   },
   {
