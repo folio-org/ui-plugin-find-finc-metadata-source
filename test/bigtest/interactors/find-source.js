636f44afa70057512fa6c909b6ede99a782b0a27
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-09-14 16:29:49 +0200

    UIFC-217-bigtest add test for searchbox and filter

--- test/bigtest/interactors/find-source.js
+++ test/bigtest/interactors/find-source.js
@@ -1,15 +1,38 @@
 import {
   interactor,
   is,
-  // isPresent,
   scoped,
   collection,
-  clickable
+  clickable,
+  focusable,
+  fillable,
+  blurrable,
+  text,
 } from '@bigtest/interactor';
 
+@interactor class SearchField {
+  static defaultScope = '#sourceSearchField';
+  isFocused = is(':focus');
+  focus = focusable();
+  fill = fillable();
+  blur = blurrable();
+  value = text();
+}
+
 @interactor class PluginModalInteractor {
+  // static defaultScope = '#ModuleContainer';
+  statusFilter = scoped('section[id="filter-accordion-status"]');
+  solrShardFilter = scoped('section[id="filter-accordion-solrShard"]');
+
   instances = collection('[role="rowgroup"] [role="row"]', {
     click: clickable('[role=gridcell]'),
+  });
+
+  searchField = scoped('#sourceSearchField', SearchField);
+  searchFocused = is('#sourceSearchField', ':focus');
+  searchButton = scoped('#sourceSubmitSearch', {
+    click: clickable(),
+    isEnabled: is(':not([disabled])'),
   });
 }
 
--- test/bigtest/tests/find-source-test.js
+++ test/bigtest/tests/find-source-test.js
@@ -42,5 +42,41 @@ describe('UI-plugin-find-source', function () {
         findSource.button.isPresent
       ).to.be.true;
     });
+
+    describe('click the button', function () {
+      beforeEach(async function () {
+        await findSource.button.click();
+      });
+
+      it('opens a modal', function () {
+        expect(
+          findSource.modal.isPresent
+        ).to.be.true;
+      });
+
+      it('focuses the search field', function () {
+        expect(
+          findSource.modal.searchField.isFocused
+        ).to.be.true;
+      });
+
+      it('status filter should be present', function () {
+        expect(findSource.modal.statusFilter.isPresent).to.be.true;
+      });
+
+      it('solrShard filter should be present', function () {
+        expect(findSource.modal.solrShardFilter.isPresent).to.be.true;
+      });
+
+      // describe('checking show inactive filter', function () {
+      //   beforeEach(async function () {
+      //     await findSource.modal.clickInactiveUsersCheckbox();
+      //   });
+
+      //   it('pulls a result set', function () {
+      //     expect(findSource.modal.instances().length).to.be.greaterThan(0);
+      //   });
+      // });
+    });
   });
 });
