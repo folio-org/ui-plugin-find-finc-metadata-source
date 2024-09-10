c345727b6eeaaf8c047f60e1966a4aba3b301a06
elsenhans <48911833+elsenhans@users.noreply.github.com>
2024-09-10 08:39:31 +0200

    UIFC-369-remove-dependency-to-stripes-acq-components (#95)
    Co-authored-by: elsenhans <elsenhans@users.noreply.github.com>

--- test/jest/__mock__/index.js
+++ test/jest/__mock__/index.js
@@ -1,15 +1,6 @@
-// import '@folio/stripes-acq-components/test/jest/__mock__';
-
-import '@folio/stripes-acq-components/test/jest/__mock__/beautifulDND.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/currencyData.mock';
-// import '@folio/stripes-acq-components/test/jest/__mock__/documentCreateRange.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/localStorage.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/matchMedia.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/reactIntl.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/reactVirtualizedAutoSize.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/stripesConfig.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/stripesCore.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/stripesIcon.mock';
-import '@folio/stripes-acq-components/test/jest/__mock__/stripesSmartComponents.mock';
-
+import './currencyData.mock';
+import './reactIntl.mock';
+import './stripesConfig.mock';
+import './stripesCore.mock';
+import './stripesIcon.mock';
 import './intl.mock';
